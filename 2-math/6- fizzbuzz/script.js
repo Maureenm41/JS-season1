d7af2cfe4d18d67d1aba25e9882c1ac0b509a381
Maureenm41 <maureenmrchl@gmail.com>
2021-02-02 14:11:38 +0100

    test 2.6

--- 2-math/6- fizzbuzz/script.js
+++ 2-math/6- fizzbuzz/script.js
@@ -5,3 +5,17 @@
     // your code here
 
 })();
+
+//ex 3
+
+console.log(
+    Array.apply(0, Array(100)).map(function (x, y) {
+    var nb = y+1;
+    var isFizz = ((nb % 3) == 0);
+    var isBuzz = ((nb % 5) == 0);
+    var isFizzBuzz = (isFizz && isBuzz);
+    if (isFizzBuzz) return 'fizzbuzz';
+    if (isFizz) return 'fizz';
+    if (isBuzz) return 'buzz';
+    return(nb);
+    }).join("\n"));
