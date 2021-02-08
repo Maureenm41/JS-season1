1a4b1e3c921a50d5b42da4002a294a02fe66bf93
Maureenm41 <maureenmrchl@gmail.com>
2021-02-08 10:26:24 +0100

    7.4

---
+++ 7-classes/4- getter setter/script.js
@@ -0,0 +1,13 @@
+// 07-classes/04-getter-setter/script.js - 7.4: getter & setter
+
+
+(() => {
+    // your code here
+})();
+
+function person
+{
+    var firstName = "Maureen" ;
+    var lastName = "Marchal" ;
+
+}
