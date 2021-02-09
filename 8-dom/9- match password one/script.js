df2a0ce5ada78b7af776e3d8f08f9e9ee6fd1e12
Maureenm41 <maureenmrchl@gmail.com>
2021-02-09 14:42:01 +0100

    8.9

--- 8-dom/9- match password one/script.js
+++ 8-dom/9- match password one/script.js
@@ -5,6 +5,32 @@
 
     // your code here
 
-    const name = document.getElementById("pass-one")
+    const passOne = document.getElementById("pass-one")
+    const passTwo = document.getElementById("pass-two")
+
+    const form = document.getElementById("formPassword")
+
+    const errorElement = document.getElementById()
+
+    form.addEventListener("Run the script", (e) =>
+    {
+        let messages =[]
+        if (passOne.value === '' || passOne.value == 0)
+        {
+            messages.push('password is required')
+        }
+
+        if (passOne.value.length <= 6)
+        {
+            messages.push('Password must be longer')
+        }
+
+        if (messages.length > 0)
+        {
+            e.preventDefault()
+            errorElement.innerText = messages.join(',')
+        }
+        
+    })
 
 })();
