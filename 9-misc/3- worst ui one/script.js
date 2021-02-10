fe29ef549fb2900d9826f2a2840005c00caa4a84
Maureenm41 <maureenmrchl@gmail.com>
2021-02-10 15:28:35 +0100

    9.3

---
+++ 9-misc/3- worst ui one/script.js
@@ -0,0 +1,12 @@
+// 09-misc/03-worst-ui-one/script.js - 9.3: worst user interface (1) : phone slider
+
+
+(() => {
+
+    // your code here
+
+})();
+
+function updateTextInput(val) {
+    document.getElementById('target').value=val; 
+  }
