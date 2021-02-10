09a40b782fac8b3a8e7fb49fbb37e59642fc3f2e
Maureenm41 <maureenmrchl@gmail.com>
2021-02-10 11:56:34 +0100

    9.1

--- 9-misc/1- waving text/script.js
+++ 9-misc/1- waving text/script.js
@@ -6,3 +6,23 @@
     // your code here
 
 })();
+
+const text = document.querySelector(".material");
+const strtext = text.textContent ;
+const splitText = strtext.split("") ;
+
+text.textContent="";
+
+for (let i=0; i < splitText.length; i++ )
+{
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50) ;
+
+function onTick()
+{
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('')
+}
