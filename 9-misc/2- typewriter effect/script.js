628eb517b9652e9619a4a40d55e9c3f8e14bfd39
Maureenm41 <maureenmrchl@gmail.com>
2021-02-10 15:21:16 +0100

    9.2

--- 9-misc/2- typewriter effect/script.js
+++ 9-misc/2- typewriter effect/script.js
@@ -16,4 +16,23 @@ const typeWriter = function(txtElement, words ,wait = 1000)
     this.wordIndex = 0 ;
     this.wait = parseInt( wait, 10);
     this.type();
+    this.isDeleting = false;
+}
+
+typeWriter.prototype = function()
+{
+    console.log(hello)
+    setTimeout(() =>this.type(), 500)
+}
+
+document.addEventListener("DOMContentLoaded" , init );
+
+function init()
+{
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words')) ;
+    const wait = txtElement.getAttribute('data-wait') ;
+
+    new typeWriter(txtElement, words , wait);
+
 }
