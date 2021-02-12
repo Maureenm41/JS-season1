cc43483508530ec7b9e6189d47a33f9f41a9a38f
Maureenm41 <maureenmrchl@gmail.com>
2021-02-12 11:33:07 +0100

    9.6

--- 9-misc/6- guess number/script.js
+++ 9-misc/6- guess number/script.js
@@ -6,3 +6,25 @@
     // your code here
 
 })();
+
+let btn = document.getElementById('btn');
+let output = document.getElementById('outputtext');
+
+let number = [Math.floor(Math.random()*100)]
+
+btn.addEventListener('click', function()
+{
+    let input = document.getElementById('userInput').value;
+    if(input == number)
+    {
+        output.innerHTML = `that's it! The number was ${number}`;
+    }
+    else if (input < number)
+    {
+        output.innerHTML = "higher";
+    };
+    if (input > number)
+    {
+        output.innerHTML ="lower";
+    }
+})
